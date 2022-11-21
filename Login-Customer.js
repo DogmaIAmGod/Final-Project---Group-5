ef10b620eef62c0dd4248ad491bc0173ea50f71f
DogmaIAmGod <douglas.t.james.iii@gmail.com>
2022-11-21 16:32:28 -0500

    Worked on more login features

--- Login-Customer.js
+++ Login-Customer.js
@@ -1,64 +1,86 @@
 let loginInformation = [
     {
-        "Username" : "Admin",
-        "Password" : "Admin123",
-        "Email" : "admin@admin.com"
+        "Username": "Admin",
+        "Password": "Admin123",
+        "Email": "admin@admin.com"
     },
     {
-        "Username" : "sampleCustomer",
-        "Password" : "password123",
-        "Email" : "sample@customer.com",
+        "Username": "sampleCustomer",
+        "Password": "password123",
+        "Email": "sample@customer.com",
     }
 ]
 
-function clearText(value) {
-value.value = ""
+//check if they want the newsletter
+let newsletterCheck = 0
+
+//when page loads, clear the value of inputs
+function emptyForm() {
+    let array = []
+    array.push(document.getElementById("signupEmail"))
+    array.push(document.getElementById("signupUsername"))
+    array.push(document.getElementById("signupPassword"))
+    array.push(document.getElementById("checkSignupPassword"))
+    array.push(document.getElementById("loginUsername"))
+    array.push(document.getElementById("loginPassword"))
+    document.getElementById("news").checked = false
+
+    for (let i = 0; i < array.length; i++) {
+        array[i].value = ""
+    }
+
+
+
 }
 
+//function to clear the text in a field
+function clearText(value) {
+    value.value = ""
+}
+
+//testing function
 function thisIsJustATest() {
     let x = document.getElementById("testing")
     let y = document.getElementById("loginPassword")
     x.innerHTML = y.value
 }
 
+//Switch between the login and sign up pages
 function loginSignupSwitcher() {
-        let x = document.getElementById("login")
-        let y = document.getElementById("signup")
-    
+    let x = document.getElementById("login")
+    let y = document.getElementById("signup")
     if (x.style.display === "none") {
-    y.style.display = "none"
-    x.style.display = "block"
-    console.log("left")
+        y.style.display = "none"
+        x.style.display = "block"
     }
     else {
-    x.style.display = "none"
-    y.style.display = "block"
-    console.log("right")
-    }   
+        x.style.display = "none"
+        y.style.display = "block"
+    }
 }
 
+
+//Check if there is text in the fields
+//Check fields against loginInformation (variable holding usernames, password, and emails) then logs you in
 function checkLogin() {
     let x = document.getElementById("loginUsername").value;
-    // console.log(x)
-    let y = document.getElementById("loginPassword").value
-    // console.log(y)
+    let y = document.getElementById("loginPassword").value;
 
     if (x === "") {
-        alert("You need to enter a username silly")
+        alert("You need to enter a username or email, silly")
         x = document.getElementById("loginUsername")
         x.focus()
     }
-    
     else if (y === "") {
-        alert("You need to enter a password silly")
+        alert("You need to enter a password, silly")
         y = document.getElementById("loginPassword")
         y.focus()
     }
     else {
-        for(let i=0; i < loginInformation.length; i++) {
+        for (let i = 0; i < loginInformation.length; i++) {
             x = document.getElementById("loginUsername")
             y = document.getElementById("loginPassword")
-            if (loginInformation[i].Username.toLowerCase() === x.value && loginInformation[i].Password.toLowerCase() === y.value) {
+            if ((loginInformation[i].Username.toLowerCase() === x.value || loginInformation[i].Email.toLowerCase() === x.value) && loginInformation[i].Password.toLowerCase() === y.value) {
                 console.log("login successful")
                 break;
             }
@@ -67,21 +89,12 @@ function checkLogin() {
                 y.value = ""
                 alert("Invalid Login")
                 break;
-                
             }
         }
     }
 }
 
-// function showPassword() {
-//     let x = document.getElementById("loginPassword");
-//     if (x.type === "password") {
-//         x.type = "text";
-//     } else {
-//         x.type = "password"
-//     }
-// }
-
+//Set up switching the eye, black dots, and letters of password
 function showPassword() {
     let x = document.getElementById("loginPassword")
     let y = document.getElementById("togglePassword")
@@ -89,4 +102,58 @@ function showPassword() {
     let type = x.getAttribute("type") === "password" ? "text" : "password";
     y.setAttribute("class", switchEye);
     x.setAttribute("type", type);
+}
+
+//Press enter key for login
+function enterLogin(e) {
+    if (e.keyCode === 13) {
+        checkLogin();
+    }
+}
+
+function checkSignup() {
+    let email = document.getElementById("signupEmail");
+    let username = document.getElementById("signupUsername");
+    let password = document.getElementById("signupPassword");
+    let checkPassword = document.getElementById("checkSignupPassword")
+    if (!email.value.includes("@") || !email.value.includes(".")) {
+        alert("This is not an email!")
+        email.focus()
+    }
+    else if (username.value.length < 8) {
+        alert("This username is less than 8 characters!")
+        username.focus()
+    }
+    else if (password.value.length < 8) {
+        alert("This password is less than 8 characters!")
+        password.focus()
+        password.value = ""
+        checkPassword.value = ""
+    }
+    else if (password.value !== checkPassword.value) {
+        alert("Passwords do not match!")
+        password.focus()
+        password.value = ""
+        checkPassword.value = ""
+    }
+
+    else {
+        //check for newsletter
+        let z = document.getElementById("news").checked
+        if (z === true) {
+            newsletterCheck = 1
+        }
+
+        //add information to array
+        let x = {
+            "Username": username.value,
+            "Password": password.value,
+            "Email": email.value
+        }
+        loginInformation.push(x)
+        loginSignupSwitcher()
+        let y = document.getElementById("loginUsername")
+        y.value = x.Username
+    }
+
 }
