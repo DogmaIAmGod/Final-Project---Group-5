94c620a757f54f3c43e7c7e03552c267a9163ce2
DogmaIAmGod <douglas.t.james.iii@gmail.com>
2022-12-12 10:55:46 -0500

    math
    Made it so that a small bug from JS no longer happens

--- Login-Customer.js
+++ Login-Customer.js
@@ -324,7 +324,7 @@ function listenForPurchase(event) {
 function number_test(n) {
     let result = (n - Math.floor(n)) !== 0;
     if (result)
-        return "$" + runningSum + "0";
+        return "$" + Math.round(runningSum * 100) / 100 + "0";
     else
         return "$" + runningSum + ".00";
 }
